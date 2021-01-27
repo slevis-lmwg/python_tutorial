2e54071b1e675e2c19ed4fa60cacffe086f8332b
Samuel Levis <slevisconsulting@gmail.com>
2021-01-27 14:11:48 -0700

    Creating mysci pkg after some other refactoring

--- heatindexcomp.py
+++ heatindexcomp.py
@@ -1,5 +1,7 @@
 
-from readdata import read_data
+from mysci.readdata import read_data
+from mysci.printing import print_comparison
+from mysci.computation import compute_heatindex
 
 # Column names and column indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'humout':5, 'heatindex':13}
@@ -15,37 +17,11 @@ for column in columns:
 # Read data from file
 data = read_data(columns, types=types)
 
-# Compute the heat index (hope I copied constants correctly)
-def compute_heatindex(t, hum):
-    a = -42.379
-    b = 2.04901523
-    c = 10.14333127
-    d = 0.22475541
-    e = 0.00683783
-    f = 0.05481717
-    g = 0.00122874
-    h = 0.00085282
-    i = 0.00000199
-
-    rh = hum / 100
-
-    hi = (a + (b * t) + (c * rh) + (d * t * rh) + (e * t ** 2) + 
-          (f * rh ** 2) + (g * t ** 2 * rh) + (h * t * rh ** 2) +
-          (i * t ** 2 * rh ** 2))
-
-    return hi
-
-# Running the function to compute heat index
+# Compute the heat index
 heatindex = []
 for temp, humout in zip(data['tempout'], data['humout']):
     heatindex.append(compute_heatindex(temp, humout))
 
 # Output comparison of data
-print('                ORIGINAL  COMPUTED')
-print(' DATE    TIME  HEAT INDX HEAT INDX DIFFERENCE')
-print('------- ------ --------- --------- ----------')
-zip_data = zip(data['date'], data['time'], data['heatindex'], heatindex)
-for date, time, hi_orig, hi_comp in zip_data:
-    hi_diff = hi_orig - hi_comp
-    print(f'{date} {time:>6} {hi_orig:9.6f} {hi_comp:9.6f} {hi_diff:10.6f}')
+print_comparison('heatindex', data['date'], data['time'], data['heatindex'], heatindex)
 
--- windchillcomp.py
+++ windchillcomp.py
@@ -1,5 +1,7 @@
 
-from readdata import read_data
+from mysci.readdata import read_data
+from mysci.printing import print_comparison
+from mysci.computation import compute_windchill
 
 # Column names and column indices to read
 columns = {'date':0, 'time':1, 'tempout':2, 'windspeed':7, 'windchill':12}
@@ -11,29 +13,10 @@ types = {'tempout': float, 'windspeed':float, 'windchill':float}
 data = read_data(columns, types=types)
 
 # Compute the wind chill temperature
-def compute_windchill(t, v):
-    a = 35.74  # empirical constants
-    b = 0.6215  # this formula fits best for temp < 45 F
-    c = 35.75
-    d = 0.4275
-
-    v16 = v ** 0.16
-
-    wci = a + b * t - c * v16 + d * t * v16
-
-    return wci
-
-# Running the function to compute wci
 windchill = []
 for temp, windspeed in zip(data['tempout'], data['windspeed']):
     windchill.append(compute_windchill(temp, windspeed))
 
 # Output comparison of data
-print('                ORIGINAL  COMPUTED')
-print(' DATE    TIME  WINDCHILL WINDCHILL DIFFERENCE')
-print('------- ------ --------- --------- ----------')
-zip_data = zip(data['date'], data['time'], data['windchill'], windchill)
-for date, time, wc_orig, wc_comp in zip_data:
-    wc_diff = wc_orig - wc_comp
-    print(f'{date} {time:>6} {wc_orig:9.6f} {wc_comp:9.6f} {wc_diff:10.6f}')
+print_comparison('windchill', data['date'], data['time'], data['windchill'], windchill)
 
