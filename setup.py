4a981eb83e1f35ec2d899c793f9d993c62189cb1
Samuel Levis <slevisconsulting@gmail.com>
2021-02-10 13:46:42 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Xdev and slevisconsulting",
+    author_email="slevisconsulting@gmail.com",
+    packages=["mysci"],
+    install_requires=[])
+
