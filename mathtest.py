b2722e3bad431f6a5ae64e9e72edaa6de6be9866
Samuel Levis <slevisconsulting@gmail.com>
2021-02-10 13:37:28 -0700

    Testing math functionality

---
+++ mathtest.py
@@ -0,0 +1,21 @@
+import math as m
+
+e = m.e
+print(e)
+
+y_natural = m.log(e)
+y_base10 = m.log(e, 10)
+y_log10 = m.log10(e)
+print(y_natural, y_base10, y_log10)
+
+deg = 180
+rads = deg * m.pi / 180
+rads_fromfunc = m.radians(deg)
+print(deg, rads, rads_fromfunc)
+
+cos_deg = m.cos(deg)  # WRONG
+cos_rad = m.cos(m.radians(deg))  # CORRECT
+print(cos_deg, cos_rad)
+
+print(m.factorial(5))
+
